2c16f17a02a01c9fc289f25185cb87e5441707bc
sophiacontesini <sophiamarques214@gmail.com>
2022-02-18 18:41:48 -0300

    requisito 07 ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
-import PlanetCard from './components/PlanetCard';
 import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
 import Title from './components/Title';
 
 class App extends React.Component {
@@ -10,8 +10,8 @@ class App extends React.Component {
       <div>
         <Header />
         <SolarSystem />
+        <Missions />
         <Title />
-        <PlanetCard />
       </div>
     );
   }
