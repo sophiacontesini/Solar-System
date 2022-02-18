703bc79f64501d86221811da909f68de6c82bd3e
sophiacontesini <sophiamarques214@gmail.com>
2022-02-18 19:05:09 -0300

    requisito 09 ok

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -3,8 +3,7 @@ import React from 'react';
 
 class PlanetCard extends React.Component {
   render() {
-    const { planetName } = this.props;
-    const { planetImage } = this.props;
+    const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
         <p data-testid="planet-name">{planetName}</p>
