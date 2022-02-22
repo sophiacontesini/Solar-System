44aea6b82470cdf0dea0e8be3393637988edbe29
sophiacontesini <sophiamarques214@gmail.com>
2022-02-22 17:53:27 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
-import Header from './components/Header';
+import Header from './components/header';
 import './App.css';
 
 class App extends React.Component {
